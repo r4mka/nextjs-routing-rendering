bdaa425bb0bcbea861e81bf158ad23182fce6b42
Adam Ramski <adam.ramski@gmail.com>
2026-06-09 23:55:15 +0200

    add error handling to /archive pages

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -27,6 +27,14 @@ export default async function FilteredNewsPage({ params }) {
     links = [];
   }
 
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter!");
+  }
+
   return (
     <>
       <header id="archive-header">
