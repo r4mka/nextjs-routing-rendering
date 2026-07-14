238d2d9803594ec7936e8ac3891502601d3bc211
Adam Ramski <adam.ramski@gmail.com>
2026-07-14 19:09:21 +0200

    add /news/[slug]/image page

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,5 +1,7 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+
+import { DUMMY_NEWS } from "@/dummy-news";
 
 export default async function NewsDetailPage({ params }) {
   const { slug } = await params;
@@ -12,7 +14,9 @@ export default async function NewsDetailPage({ params }) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
